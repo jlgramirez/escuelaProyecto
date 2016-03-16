f50f748e57d1c204f38c8b9f720b5885f8190686
Jose Luis Gonzalez <jlgonza07@gmail.com>
2016-03-16 19:09:13 +0100

    hecho merge

--- jarra/src/jarra/jarra.java
+++ jarra/src/jarra/jarra.java
@@ -20,6 +20,12 @@ public class jarra {//variables de instancia
 		return this.cantidad;
 	}
              // 2
+        public void llena(){
+		this.cantidad=this.capacidad;
+	}
+	public void vacia(){
+		this.cantidad=0;
+	}
              // 3
         public void metodoComunitario(){ 
 // Comentario incluido para pruebas 
